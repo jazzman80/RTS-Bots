230917a7fda0d96478ae30585c780fac28efed92
Ivan <jazzman.ain@gmail.com>
2021-09-06 22:38:00 +0300

    Robot State Machine

--- Assets/#Game/Scripts/Robot.cs
+++ Assets/#Game/Scripts/Robot.cs
@@ -5,10 +5,20 @@ using UnityEngine.AI;
 
 public class Robot : MonoBehaviour
 {
+    private enum State
+    {
+        idle,
+        moveToPoint,
+        moveToStorage,
+        moveToFabric,
+        moveToShelter
+    }
+    
     [SerializeField] GameObject selectMarker;
     [SerializeField] NavMeshAgent agent;
 
     bool selected = false;
+    State state = State.idle;
 
     private void Update()
     {
@@ -25,6 +35,28 @@ public class Robot : MonoBehaviour
         if(Physics.Raycast(ray, out hit))
         {
             agent.SetDestination(hit.point);
+
+            string objectType = hit.collider.gameObject.tag;
+            Debug.Log("I'm move to " + objectType);
+
+            switch (objectType)
+            {
+                case "Terrain":
+                    state = State.moveToPoint;
+                    break;
+
+                case "Fabric":
+                    state = State.moveToFabric;
+                    break;
+
+                case "Storage":
+                    state = State.moveToStorage;
+                    break;
+
+                case "Shelter":
+                    state = State.moveToShelter;
+                    break;
+            }
         }
     }
 
