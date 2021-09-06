072ac2877c5f88cbd32ba2e16e092475a62d86f4
Ivan <jazzman.ain@gmail.com>
2021-09-06 21:52:26 +0300

    Robot Move To Point

--- Assets/#Game/Scripts/Robot.cs
+++ Assets/#Game/Scripts/Robot.cs
@@ -1,12 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Robot : MonoBehaviour
 {
     [SerializeField] GameObject selectMarker;
+    [SerializeField] NavMeshAgent agent;
 
     bool selected = false;
+
+    private void Update()
+    {
+        if (selected && Input.GetMouseButtonDown(1)) SetTarget();
+    }
+
+    private void SetTarget()
+    {
+        Deselect();
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            agent.SetDestination(hit.point);
+        }
+    }
 
     private void OnMouseDown()
     {
