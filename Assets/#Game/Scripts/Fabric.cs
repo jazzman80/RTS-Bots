7fe2b7fd85e37a48306759ee4fc84d1f368b9fe2
Ivan <jazzman.ain@gmail.com>
2021-09-14 18:39:40 +0300

    Robot Dust VFX

--- Assets/#Game/Scripts/Fabric.cs
+++ Assets/#Game/Scripts/Fabric.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class Fabric : Building
 {
+    [SerializeField] ParticleSystem recycleFx;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Box"))
         {
             Box box = other.GetComponent<Box>();
-
             box.LinkToFabric(this.TargetPoint);
+            recycleFx.Play();
         }
 
         if (other.CompareTag("Robot"))
